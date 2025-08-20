c852b43ec040f5a16e99a93e5ca10535013ffdbc
shiraiwa_kazuki <shiraiwa.kazuki@alhinc.jp>
2025-08-20 14:58:07 +0900

    スタイルシートを設定 以下の機能を実装 　ユーザー登録/ログイン/ユーザー情報表示/つぶやき/メッセージの表示/ユーザー情報変更/ログアウト

--- src/chapter6/controller/SettingServlet.java
+++ src/chapter6/controller/SettingServlet.java
@@ -10,27 +10,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.StringUtils;
 
 import chapter6.beans.User;
+import chapter6.exception.NoRowsUpdatedRuntimeException;
 import chapter6.logging.InitApplication;
 import chapter6.service.UserService;
 
-@WebServlet(urlPatterns = { "/signup" })
-public class SignUpServlet extends HttpServlet {
+@WebServlet(urlPatterns = { "/setting" })
+public class SettingServlet extends HttpServlet {
 
 
-   /**
-   * ロガーインスタンスの生成
-   */
+	/**
+	* ロガーインスタンスの生成
+	*/
     Logger log = Logger.getLogger("twitter");
 
     /**
     * デフォルトコンストラクタ
     * アプリケーションの初期化を実施する。
     */
-    public SignUpServlet() {
+    public SettingServlet() {
         InitApplication application = InitApplication.getInstance();
         application.init();
 
@@ -38,31 +40,48 @@ public class SignUpServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
+            throws ServletException, IOException {
 
 	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
         " : " + new Object(){}.getClass().getEnclosingMethod().getName());
 
-        request.getRequestDispatcher("signup.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        User loginUser = (User) session.getAttribute("loginUser");
+
+        User user = new UserService().select(loginUser.getId());
+
+        request.setAttribute("user", user);
+        request.getRequestDispatcher("setting.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-
+            throws ServletException, IOException {
 
 	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
         " : " + new Object(){}.getClass().getEnclosingMethod().getName());
 
+        HttpSession session = request.getSession();
         List<String> errorMessages = new ArrayList<String>();
 
         User user = getUser(request);
-        if (!isValid(user, errorMessages)) {
+        if (isValid(user, errorMessages)) {
+            try {
+                new UserService().update(user);
+            } catch (NoRowsUpdatedRuntimeException e) {
+		    log.warning("他の人によって更新されています。最新のデータを表示しました。データを確認してください。");
+                errorMessages.add("他の人によって更新されています。最新のデータを表示しました。データを確認してください。");
+            }
+        }
+
+        if (errorMessages.size() != 0) {
             request.setAttribute("errorMessages", errorMessages);
-            request.getRequestDispatcher("signup.jsp").forward(request, response);
+            request.setAttribute("user", user);
+            request.getRequestDispatcher("setting.jsp").forward(request, response);
             return;
         }
-        new UserService().insert(user);
+
+        session.setAttribute("loginUser", user);
         response.sendRedirect("./");
     }
 
@@ -73,6 +92,7 @@ public class SignUpServlet extends HttpServlet {
         " : " + new Object(){}.getClass().getEnclosingMethod().getName());
 
         User user = new User();
+        user.setId(Integer.parseInt(request.getParameter("id")));
         user.setName(request.getParameter("name"));
         user.setAccount(request.getParameter("account"));
         user.setPassword(request.getParameter("password"));
@@ -95,17 +115,14 @@ public class SignUpServlet extends HttpServlet {
         if (!StringUtils.isEmpty(name) && (20 < name.length())) {
             errorMessages.add("名前は20文字以下で入力してください");
         }
-
         if (StringUtils.isEmpty(account)) {
             errorMessages.add("アカウント名を入力してください");
         } else if (20 < account.length()) {
             errorMessages.add("アカウント名は20文字以下で入力してください");
         }
-
         if (StringUtils.isEmpty(password)) {
             errorMessages.add("パスワードを入力してください");
         }
-
         if (!StringUtils.isEmpty(email) && (50 < email.length())) {
             errorMessages.add("メールアドレスは50文字以下で入力してください");
         }
--- src/chapter6/controller/TopServlet.java
+++ src/chapter6/controller/TopServlet.java
@@ -1,6 +1,7 @@
 package chapter6.controller;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -9,12 +10,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import chapter6.beans.User;
+import chapter6.beans.UserMessage;
 import chapter6.logging.InitApplication;
+import chapter6.service.MessageService;
 
 
 @WebServlet(urlPatterns = { "/index.jsp" })
 public class TopServlet extends HttpServlet {
-
+	private static final long serialVersionUID = 1L;
 
     /**
     * ロガーインスタンスの生成
@@ -38,6 +42,18 @@ public class TopServlet extends HttpServlet {
 
 	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
         " : " + new Object(){}.getClass().getEnclosingMethod().getName());
-        request.getRequestDispatcher("/top.jsp").forward(request, response);
+
+	  //フラグ管理（ログインしていればOn、ログオフならOff）
+	  boolean isShowMessageForm = false;
+      User user = (User) request.getSession().getAttribute("loginUser");
+      if (user != null) {
+          isShowMessageForm = true;
+      }
+
+      List<UserMessage> messages = new MessageService().select();
+
+      request.setAttribute("messages", messages);
+      request.setAttribute("isShowMessageForm", isShowMessageForm);
+      request.getRequestDispatcher("/top.jsp").forward(request, response);
     }
 }
